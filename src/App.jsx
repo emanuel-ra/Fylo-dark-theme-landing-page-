8375ae02069954543127792bb4140cba87010376
Tomas Emanuel Ramirez Abarca <emanuelrr55@gmail.com>
2022-09-28 01:05:33 -0500

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Navbar from "./components/Navbar";
 import Body from "./components/Body"
 
+import desktop from "./assets/bg-curvy-desktop.svg"
+import mobile from "./assets/bg-curvy-mobile.svg"
+
 function App() {
   return (
     <>
