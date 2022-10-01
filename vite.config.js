bba922b56be6c33a6129a2093d7f6b7aafe1adb4
Tomas Emanuel Ramirez Abarca <emanuelrr55@gmail.com>
2022-10-01 09:59:43 -0500

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()] ,
-  base: '/Fylo-dark-theme-landing-page-/'
+  plugins: [react()] //,
+  //base: '/Fylo-dark-theme-landing-page-/'
 })
